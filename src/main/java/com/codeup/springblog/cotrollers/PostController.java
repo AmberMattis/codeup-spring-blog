b5b3eeef43fd6c01cc78a69e0555922e60a40159
Amber Mattis <amber.mattis2@gmail.com>
2021-02-11 10:16:56 -0600

    built out views for posts

--- src/main/java/com/codeup/springblog/cotrollers/PostController.java
+++ src/main/java/com/codeup/springblog/cotrollers/PostController.java
@@ -1,22 +1,43 @@
 package com.codeup.springblog.cotrollers;
 
+import com.codeup.springblog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
 
+
     @GetMapping("/posts")
-    @ResponseBody
-    public String post() {
-        return "Posts Index Page";
+    public String post(Model model) {
+        Post post1 = new Post("My first post", "This is my first post", 1L);
+        Post post2 = new Post("My second post","This is my second post", 2L);
+        Post post3 = new Post("My third post", "This is my third post", 2L);
+
+        List<Post> allPosts = new ArrayList<>();
+        allPosts.add(post1);
+        allPosts.add(post2);
+        allPosts.add(post3);
+
+        model.addAttribute("title", "All Posts");
+        model.addAttribute("posts", allPosts);
+
+        return "posts/index";
     }
 
 
+
+
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String postById(@PathVariable int id) {
-        return "View individual post " + id;
+    public String postById(Model model) {
+        Post post1 = new Post("My first post", "This is my first post", 1L);
+        model.addAttribute("title", "Specific Post");
+        model.addAttribute("post", post1);
+        return "posts/show";
     }
 
 
