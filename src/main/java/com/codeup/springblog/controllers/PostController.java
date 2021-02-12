87ab085631d57f743352fc421c547a4c46e86a6b
Amber Mattis <amber.mattis2@gmail.com>
2021-02-12 14:45:37 -0600

    delete post functionality complete

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -8,11 +8,15 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PostController {
+
+
     private final PostRepository postsDao;
 
     public PostController(PostRepository postDao) {
         this.postsDao = postDao;
     }
+
+
 
 
     @GetMapping("/posts")
@@ -39,7 +43,7 @@ public class PostController {
 
     @GetMapping("/posts/create")
     @ResponseBody
-    public String viewFrom() {
+    public String viewFromCreate() {
         return "View the form for creating post";
     }
 
@@ -50,6 +54,47 @@ public class PostController {
 
         return "Create a new posts";
     }
+
+
+
+//    @GetMapping("/posts/delete")
+//    public String viewFormDelete(Model model){
+//        model.addAttribute("post", postsDao.deleteById());
+//        return "post/index";
+//    }
+
+
+//    @PostMapping("/posts/delete")
+//    public String deleteForm(@ModelAttribute("post") Post post) {
+//     postsDao.deleteById(post.getId());
+//    return "posts/index";
+//    }
+
+
+    @PostMapping("/posts/delete")
+    public String deleteForm(@ModelAttribute("delete") Post post, Model model) {
+
+        postsDao.deleteById(post.getId());
+        model.addAttribute("title", post.getTitle());
+        return "posts/delete";
+    }
+
+
+
+
+    @GetMapping("/posts/edit")
+    @ResponseBody
+    public String viewFormEdit(){
+        return "View form for editing a post";
+    }
+
+    @PostMapping("/posts/edit")
+    @ResponseBody
+    public String editForm(){
+        return "Edit existing post";
+    }
+
+
 
 
 
