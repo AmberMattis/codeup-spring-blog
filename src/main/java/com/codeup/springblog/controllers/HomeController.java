7ae4395898d2b037da38e4b30114be4a21a26f49
Amber Mattis <amber.mattis2@gmail.com>
2021-02-27 17:56:03 -0600

    added a featured post section to landing page

--- src/main/java/com/codeup/springblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -1,33 +1,43 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Post;
 import com.codeup.springblog.models.User;
 import com.codeup.springblog.repositories.PostRepository;
 import com.codeup.springblog.repositories.UserRepository;
+import org.jeasy.random.EasyRandom;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 @Controller
 public class HomeController {
 
     private final UserRepository usersDao;
     private final PasswordEncoder encoder;
+    private final PostRepository postsDao;
 
-    public HomeController(UserRepository usersDao, PasswordEncoder encoder ){
+    public HomeController(UserRepository usersDao, PasswordEncoder encoder, PostRepository postsDao ){
         this.usersDao = usersDao;
         this.encoder = encoder;
+        this.postsDao = postsDao;
 
     }
 
-    @GetMapping("/")
-
-
-    public String main(){
-        return "home";
-    }
+//    @GetMapping("/")
+//
+//
+//    public String main(){
+//        return "home";
+//    }
 
     @GetMapping("/login")
     public String showLoginForm(){
@@ -47,6 +57,15 @@ public class HomeController {
         user.setPassword(hash);
         usersDao.save(user);
         return "redirect:/login";
+    }
+
+    @GetMapping("/")
+    public String showFeaturedPosts(Model model){
+        EasyRandom generator = new EasyRandom();
+        List<Post> posts = generator.objects(Post.class, 3).collect(Collectors.toList());
+        model.addAttribute("posts", posts);
+        return "home";
+
     }
 
 
